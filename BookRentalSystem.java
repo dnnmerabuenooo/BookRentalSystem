f49b3f7cdaa85ad493c611ab0e7f63dc9ebcf2ba
Dianne Merabueno <111546446+dnnmerabuenooo@users.noreply.github.com>
2023-04-13 10:53:47 +0800

    Create BookRentalSystem.java

---
+++ BookRentalSystem.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BookRentalSystem {
+
+    public static void main(String[] args) {
+
+        String[] titles = {"System Analysis and Design", "Android Application", "Programming Concepts and Logic Formulation"};
+
+        String[] authors = {"Vincent Mullet", "Fiona Martinez", "Lanz Ricamara"};
+
+        int[] years = {2020, 2015, 2018};
+
+        int[] copies = {2, 3, 4};
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice;
+
+        while (true) {
+
+            System.out.println("Available Books:");
+
+            }
