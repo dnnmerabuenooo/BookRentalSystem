6ef7b16942220eeff0fbb8d88761008e420c6e32
Dianne Merabueno <111546446+dnnmerabuenooo@users.noreply.github.com>
2023-04-20 11:21:44 +0800

    Create BookRental.java

---
+++ Working/BookRental.java
@@ -0,0 +1,132 @@
+import java.util.ArrayList;
+
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
+
+
+
+public class BookRental {
+
+
+
+	public static void main(String[] args) {
+
+
+		ArrayList<Book> books = new ArrayList<>();
+
+
+
+		Book book1 = new Book();
+
+		book1.name = "System Analysis and Design";
+
+		book1.author = "Gary B. Shelly";
+
+		book1.copy = 2;
+
+
+
+		Book book2 = new Book();
+
+		book2.name = "Android Application";
+
+		book2.author = "Corine Hoisington";
+
+		book2.copy = 3;
+
+
+
+		Book book3 = new Book();
+
+		book3.name = "Programming Concepts and Logic Formulation";
+
+		book3.author = "Rosauro E. Manuel";
+
+		book3.copy = 4;
+
+
+
+		books.add(book1);
+
+		books.add(book2);
+
+		books.add(book3);
+
+
+
+		System.out.println("ELECTRONIC BOOK RENTAL SYSTEM");
+
+
+
+		for (int i = 0; i < books.size(); i++) {
+
+			System.out.print(i);
+
+			System.out.print(" " + books.get(i).getName() + ", " + books.get(i).getAuthor());
+
+			System.out.println();
+
+		}
+
+
+
+		boolean end = false;
+
+
+
+		while (!end) {
+
+			try {
+
+				Scanner scan = new Scanner(System.in);
+
+				System.out.print("\nChoose a number to rent a book: ");
+
+				int choice = scan.nextInt();
+
+
+
+				Book book = books.get(choice);
+
+				int copy = book.getCopy();
+
+				if (copy > 0) {
+
+					System.out.println("You rented : " + books.get(choice).getName());
+
+					book.setCopy(copy -= 1);
+
+				} else {
+
+					System.out.println("No copies left.");
+
+				}
+
+
+
+				System.out.print("\nDo you want to rent again? [Y/N]");
+
+				String ask = scan.next();
+
+				if (ask.equalsIgnoreCase("N")) {
+
+					end = true;
+
+				}
+
+			} catch (InputMismatchException e) {
+
+				System.out.println("Input a number only.");
+
+			} catch (IndexOutOfBoundsException e) {
+
+				System.out.println("Input is not in the choices.");
+
+			}
+
+		}
+
+	}
+
+}
